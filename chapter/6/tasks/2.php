6a312794406f9dbcdb0a74925345f0178724624f
Noilty (Roma Kuzmin) <thenoilty@gmail.com>
2019-11-08 18:24:08 +0700

    MyPC1: User Noilty

--- chapter/6/tasks/2.php
+++ chapter/6/tasks/2.php
@@ -2,11 +2,11 @@
 <?php
 // Задание -> 2
 // Используя константы и обслуживающие их функции, добейтесь от конструкции require поведения require_once. Иными словами, при многократном включении РНР-файла, например, с классом Point, код во включаемом файле должен выполниться только ОДИН раз. 
-// Не реализовано
+// Не реализовано (Нужна помощь)
 $array = [
     '../../5/listings/5-2/point.php',
     '../../5/listings/5-9/class_scope.php',
-    '../../5/listings/5-2/point.php',
+    '../../5/listings/5-2/point.php', // Ошибка повторного подключения
 ];
 
 myRequire($array);
@@ -17,6 +17,7 @@ function myRequire($array){
         $i++;
         $name = "file{$i}";
         define($name, $array_value);
+        
         if(defined($name)){
             require constant($name);
             echo "$name: ". constant($name);
