b0dd15b628a003bf6fa0aeb9508e4a94734ee0e7
Noilty (Roma Kuzmin) <thenoilty@gmail.com>
2019-11-09 18:24:48 +0700

    MyPC1: User Noilty | modified: chapter/8/tasks/1.php, modified: chapter/8/tasks/hello.txt, deleted: nbproject/private/config.properties, modified: nbproject/private/private.properties, modified: nbproject/project.properties

--- chapter/8/tasks/1.php
+++ chapter/8/tasks/1.php
@@ -1,19 +1,59 @@
+<meta charset='utf-8'>
 <?php
 // Задание -> 1
 // В документации на сайте http://php.net найдите описание функций file_get_сontents () и file_put_contents (), которые позволяют читать и записывать содержимое файла. Создайте два скрипта, первый должен записывать строку "Hello, world!" в файл hello.txt, а второй- считывать содержимое файла hello.txt и выводить его в окно браузера. 
 
-if(file_exists('hello.txt')){
-    echo '1';
+/**
+ * 1,1
+ */
+$file_name = 'hello.txt';
+if(file_exists($file_name)){
+    $message = 'Hello, World!';
+    echo 'Файл '.$file_name.' найден<br />';
+    if(!empty($message)){
+        echo 'Сообщение: '.$message;
+        if(getFile($file_name, $message)){
+            echo '<br />Запись: Успех';
+        } else {
+            echo '<br />Запись: Что-то пошло не так';
+        }
+    } else {
+        echo 'Пустое сообщение';
+    }
 } else {
-    echo '0';
+    echo 'Файл не найден';
 }
 
-function getFile($filename, $text) {
-    $result = file_get_contents($filename, $text);
+echo '<hr />';
+
+/**
+ * 1,2
+ */
+if(file_exists($file_name)){
+    $text = setFile($file_name);
+    echo 'Содержимое файла '.$file_name.': '.$text;
+} else {
+    echo 'Файл не найден';
+}
+
+/**
+ * Функция чтения из файла
+ * @param type $file_name
+ * @param type $message
+ * @return type
+ */
+function setFile($file_name) {
+    $result = file_get_contents($file_name);
     return $result;
 }
 
-function setFile($filename, $text) {
-    $result = file_put_contents($filename, $text);
+/**
+ * Функция записи в файл
+ * @param type $file_name
+ * @param type $message
+ * @return type
+ */
+function getFile($file_name, $message) {
+    $result = file_put_contents($file_name, $message);
     return $result;    
 }
