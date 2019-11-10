5b89d4fb0b684bad17db06a1756c72f875240906
Noilty (Roma Kuzmin) <thenoilty@gmail.com>
2019-11-10 21:10:12 +0700

    modified: chapter/11/tasks/1.php, modified: chapter/11/tasks/2.php, modified: chapter/11/tasks/3.php

--- chapter/11/tasks/3.php
+++ chapter/11/tasks/3.php
@@ -2,3 +2,20 @@
 <?php
 // Задание -> 3
 // Создайте функцию sum ( ) , которая принимает любое количество числовых аргументов и возвращает их сумму
+
+$arg = [5,5,5,5];
+
+echo sum(...$arg);
+
+/**
+ * Сумма чисел
+ * @param type $param
+ * @return type
+ */
+function sum(...$param) 
+{
+    foreach ($param as $value) {
+        $sum += $value;
+    }
+    return $sum;
+}
