917a5c572eacfd4ba1181f1bbe739e3e6fbf0418
Noilty (Roma Kuzmin) <thenoilty@gmail.com>
2019-11-11 15:09:47 +0700

    new file: chapter/12/tasks/point.txt, new file: chapter/12/tasks/rome_number.php, modified: chapter/12/tasks/2.php,  modified: chapter/12/tasks/3.php, modified: nbproject/private/private.xml

--- chapter/12/tasks/2.php
+++ chapter/12/tasks/2.php
@@ -19,13 +19,19 @@ $point = new Point();
 $point->x = 1;
 $point->y = 1;
 
+getFile($point, 'point.txt');
+
+echo "<hr />";
+
+var_dump(setFile('point.txt'));
+
 /**
  * Сериализация
  * @param type $obj
  */
 function serializeObj($obj) 
 {
-    //serialize()
+    return serialize($obj);
 }
 
 /**
@@ -34,7 +40,7 @@ function serializeObj($obj)
  */
 function deSerializeObj($obj) 
 {
-    //unserialize()
+    return unserialize($obj);
 }
 
 /**
@@ -43,7 +49,20 @@ function deSerializeObj($obj)
  */
 function getFile($obj, $name_file) 
 {
-    
+    //Проверяем существует ли файл
+    if(file_exists($name_file)){
+        echo "Файл <b>{$name_file}</b> найден<br />";
+        //Проверяем не пустой ли объект
+        if(!empty((array) $obj)){
+            echo "Объект не пустой<br />Сериализация объекта<br />Запись в файл <b>{$name_file}</b>";
+            file_put_contents($name_file, serializeObj($obj));
+        } else {
+            echo "Объект пустой";
+        }
+    } else {
+        echo "Файл <b>{$name_file}</b> не был найден и был создан автоматически";
+        fopen($name_file, 'w');
+    }
 }
 
 /**
@@ -52,5 +71,6 @@ function getFile($obj, $name_file)
  */
 function setFile($name_file) 
 {
-    
+    $file = file_get_contents($name_file);
+    return deSerializeObj($file);
 }
--- chapter/12/tasks/3.php
+++ chapter/12/tasks/3.php
@@ -1,5 +1,29 @@
 <meta charset="utf-8">
 <?php
 // Задание -> 3
-// 
+// Создайте скрипт, преобразующий число в арабской нотации (от 1 до 2000)
+// в римское. Арабские числа соотносятся с римскими следующим образом: 1 - 1,
+// 5 - V, 10- Х, 50- L, 100- С, 500- D, 1000- М. Например, 116- это
+// CXVI, 199- это CXCIX, 14-это XIV.
 
+echo romeNum(98);
+
+/**
+ * Преобразование в римские цифры
+ * @param type $number
+ * @return string
+ */
+function romeNum($number) 
+{     
+    require_once 'rome_number.php';
+    
+    $_I = $I[$number % 10];
+    $_X = $X[($number % 100) / 10];
+    $_C = $C[($number % 1000) / 100];
+    $_M = $M[$number / 1000];
+    
+    $rome_number = $_M.$_C.$_X.$_I;
+    $result = "{$number}: {$rome_number}";
+    
+    return $result;
+}
